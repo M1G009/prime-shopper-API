3430a04f4cd0b3ea3d661c9a8be24b4f8f234834
LAL009 <info@aalphatechnologies.com>
2022-02-08 12:34:34 +0530

    port issue check

--- bin/run.js
+++ bin/run.js
@@ -9,6 +9,10 @@ io = module.exports = require('socket.io')(server);
 require('../socket/socket').__init__();
 require('../socket/soc-test1')
 
-server.listen(CONFIG.PORT,CONFIG.HOST,() => {
+// server.listen(CONFIG.PORT,CONFIG.HOST,() => {
+//     console.log(`Server Running on ${CONFIG.HOST}:${CONFIG.PORT}`)
+// })
+
+server.listen(process.env.PORT || 5000,() => {
     console.log(`Server Running on ${CONFIG.HOST}:${CONFIG.PORT}`)
 })
